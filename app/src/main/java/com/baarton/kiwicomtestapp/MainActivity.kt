2fd32edcd6e264999c096be61e4d132fe000aa66
Ondras <sg.bart@seznam.cz>
2021-06-29 22:39:37 +0200

    > TODO update

--- app/src/main/java/com/baarton/kiwicomtestapp/MainActivity.kt
+++ app/src/main/java/com/baarton/kiwicomtestapp/MainActivity.kt
@@ -4,6 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.baarton.kiwicomtestapp.ui.StartFragment
 
+//TODO don't forget:
+// > tests
+// > extractions - refactoring - deleting stuff where possible
+// > annotations? UiThread?
+// > exceptions handling and logging at least
+// > layout refresh, cleanup + styles extractions
+// > permission checks? network connectivity?
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
